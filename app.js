5a17848cce421f3fe778a32a1a9b055cd0cf72c2
marloy <evgeniy.mirea@yandex.ru>
2021-03-22 20:49:19 +0300

    feat: добавить базу данных

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 const { PORT = 3000 } = process.env;
 
